7d32ad51effd99f6047b140db8d764a95258f701
Pavel Yvorskiy <pavelyvorski82@gmail.com>
2022-07-30 20:36:18 +0300

    master

---
+++ assets/script/main/card-anim.js
@@ -0,0 +1,9 @@
+VanillaTilt.init(document.querySelectorAll(".card"), {
+  max: 15,
+  speed: 3000,
+  glare: true,
+  "max-glare": 1,
+});
+
+// //It also supports NodeList
+// VanillaTilt.init(document.querySelectorAll(".your-element"));
